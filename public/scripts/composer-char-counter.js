b16badcad734d7dd7ea12e46fb59c6c635c26951
Justin <justin.s.kwok@gmail.com>
2019-05-14 22:44:18 +0000

    Added working character counter using composer-char-counter.js using jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$('textarea').on("input", function() {
+  const charsLeft = 140 - $(this).val().length;
+  $(this).siblings().text(charsLeft);
+  if (charsLeft < 0) {
+    $(this).siblings(".counter").css("color", "#ff0000")
+  } else {
+    $(this).siblings(".counter").css("color", "")
+  }
+});
+
